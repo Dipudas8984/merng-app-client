afdf9405d5352b853d3762e5809672baebc63352
dipudas8984 <dasdipu8984@gmail.com>
2021-05-11 17:54:01 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+function App() {
+  return (
+    
+      <Router>
+        <Container>
+          <h1>hello</h1>
+        </Container>
+      </Router>
+    
+  );
+}
+
+export default App;
